a77c88f79137ab561e94768c0b34c120570073ec
muksis <60219890+muksis@users.noreply.github.com>
2022-07-02 15:34:14 -0400

    Add home page and router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Navbar />
-    </>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
