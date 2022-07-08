72684ef62762e57f3ce05e131a972991b9344414
muksis <60219890+muksis@users.noreply.github.com>
2022-07-08 12:05:40 -0400

    Add userAuth to sign in page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,23 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { user, signIn } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signIn(email, password);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className="w-full h-screen">
@@ -10,10 +27,10 @@ const SignIn = () => {
             <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
               <div className="max-w-[320px] mx-auto py-16">
                 <h1 className="text-3xl font-bold ">Sign In</h1>
-                <form className="w-full flex flex-col py-4">
-                  <input className="p-3 my-2 bg-gray-700 rounded" type="email" placeholder="Email" required />
-                  <input className="p-3 my-2 bg-gray-700 rounded" type="password" placeholder="Password" required />
-                  <button className="bg-red-600 py-3 my-6 rounded font-bold">Sign Up</button>
+                <form onSubmit={handleSubmit} className="w-full flex flex-col py-4">
+                  <input onChange={(e) => setEmail(e.target.value)} className="p-3 my-2 bg-gray-700 rounded" type="email" placeholder="Email" required />
+                  <input onChange={(e) => setPassword(e.target.value)} className="p-3 my-2 bg-gray-700 rounded" type="password" placeholder="Password" required />
+                  <button className="bg-red-600 py-3 my-6 rounded font-bold">Sign In</button>
                   <div className="flex justify-between items-center text-sm text-gray-600">
                     <p>
                       <input className="mr-2" type="checkbox" />Remember me
