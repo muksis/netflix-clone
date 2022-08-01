5c165526c291ce89f61727e2a4f33b19a97c9027
muksis <60219890+muksis@users.noreply.github.com>
2022-08-01 16:00:24 -0400

    Add toastify alerts instead of clgs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
-import { Link, useNavigate } from "react-router-dom";
-import { UserAuth } from "../context/AuthContext";
+import { Link, useNavigate } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
+import { toast } from 'react-toastify';
 
 const Navbar = () => {
   const { user, logOut } = UserAuth();
@@ -8,33 +9,40 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       await logOut();
-      navigate("/");
+      navigate('/');
     } catch (error) {
-      console.log(error);
+      toast.error('Something went wrong with logout');
     }
   };
 
   return (
-    <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
-      <Link to="/">
-        <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
+    <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
+      <Link to='/'>
+        <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>
           CLONFLIX
         </h1>
       </Link>
       {user?.email ? (
         <div>
-          <Link to="/account">
-            <button className="text-white pr-4">Account</button>
+          <Link to='/account'>
+            <button className='text-white pr-4'>Account</button>
           </Link>
-            <button onClick={handleLogout} className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">Logout</button>
+          <button
+            onClick={handleLogout}
+            className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'
+          >
+            Logout
+          </button>
         </div>
       ) : (
         <div>
-          <Link to="/sign-in">
-            <button className="text-white pr-4">Sign In</button>
+          <Link to='/sign-in'>
+            <button className='text-white pr-4'>Sign In</button>
           </Link>
-          <Link to="/sign-up">
-            <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">Sign Up</button>
+          <Link to='/sign-up'>
+            <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>
+              Sign Up
+            </button>
           </Link>
         </div>
       )}
--- src/components/SavedMovies.jsx
+++ src/components/SavedMovies.jsx
@@ -1,62 +1,86 @@
-import { useState, useEffect } from "react";
-import { AiOutlineClose } from "react-icons/ai";
-import { MdChevronLeft, MdChevronRight } from "react-icons/md";
-import { UserAuth } from "../context/AuthContext";
-import { db } from "../firebase.config";
-import { updateDoc, doc, onSnapshot } from "firebase/firestore";
+import { useState, useEffect } from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { UserAuth } from '../context/AuthContext';
+import { toast } from 'react-toastify';
+import { db } from '../firebase.config';
+import { updateDoc, doc, onSnapshot } from 'firebase/firestore';
 
 const SavedMovies = () => {
-  const [movies, setMovies] = useState([[]]);
+  const [movies, setMovies] = useState([]);
 
   const { user } = UserAuth();
 
   const slideLeft = () => {
-    const slider = document.getElementById("slider");
+    const slider = document.getElementById('slider');
     slider.scrollLeft = slider.scrollLeft - 500;
   };
 
   const slideRight = () => {
-    const slider = document.getElementById("slider");
+    const slider = document.getElementById('slider');
     slider.scrollLeft = slider.scrollLeft + 500;
   };
 
-  const movieRef = doc(db, "users", `${user?.email}`);
+  const movieRef = doc(db, 'users', `${user?.email}`);
   const deleteMovie = async (movieId) => {
     try {
       const result = movies.filter((movie) => movie.id !== movieId);
       await updateDoc(movieRef, {
-        savedMovies: result
+        savedMovies: result,
       });
     } catch (error) {
-      console.log(error);
+      toast.error('Something went wrong');
     }
   };
 
   useEffect(() => {
-    onSnapshot(doc(db, "users", `${user?.email}`), (doc) => {
+    onSnapshot(doc(db, 'users', `${user?.email}`), (doc) => {
       setMovies(doc.data()?.savedMovies);
-    })
+    });
   }, [user?.email]);
 
   return (
     <div>
-      <h2 className="text-white font-bold md: text-xl p-4">Saved Movies</h2>
-      <div className="relative flex items-center group">
-        <MdChevronLeft onClick={slideLeft} className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block" size={40} />
-        <div id={'slider'} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative">
+      <h2 className='text-white font-bold md: text-xl p-4'>Saved Movies</h2>
+      <div className='relative flex items-center group'>
+        <MdChevronLeft
+          onClick={slideLeft}
+          className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+          size={40}
+        />
+        <div
+          id={'slider'}
+          className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'
+        >
           {movies.map((movie, id) => (
-            <div key={id} className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
-              <img className="w-full h-auto block" src={`https://image.tmdb.org/t/p/w500/${movie?.img}`} alt={movie?.title} />
-              <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-                <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
-                    {movie?.title}
+            <div
+              key={id}
+              className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'
+            >
+              <img
+                className='w-full h-auto block'
+                src={`https://image.tmdb.org/t/p/w500/${movie?.img}`}
+                alt={movie?.title}
+              />
+              <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
+                <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>
+                  {movie?.title}
                 </p>
-                <p onClick={() => deleteMovie(movie.id)} className="absolute text-gray-300 top-4 right-4"><AiOutlineClose /></p>
+                <p
+                  onClick={() => deleteMovie(movie.id)}
+                  className='absolute text-gray-300 top-4 right-4'
+                >
+                  <AiOutlineClose />
+                </p>
               </div>
             </div>
           ))}
         </div>
-        <MdChevronRight onClick={slideRight} className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block" size={40} />
+        <MdChevronRight
+          onClick={slideRight}
+          className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+          size={40}
+        />
       </div>
     </div>
   );
